6d976a09a3136481459a107728591c4dae87af95
JohannRodriguez <54152734+JohannRodriguez@users.noreply.github.com>
2020-03-23 14:19:36 -0700

    my_select added

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -1,20 +1,35 @@
 module Enumerable
-  def my_each(each_count)
-    each_count.length.times do |n|
-      yield(each_count[n])
+  def my_each
+    self.length.times do |n|
+      if self.is_a?(Array)
+        yield(self[n])
+      end
     end
   end
 
-  def my_each_with_index(each_with_index_count)
-    each_with_index_count.length.times do |n|
-      yield(each_with_index_count[n], n)
+  def my_each_with_index
+    self.length.times do |n|
+      yield(self[n], n)
     end
   end
 
+  def my_select
+    if self.is_a?(Array)
+      new_array = Array.new
+      self.my_each do |n|
+        if yield(n)
+          new_array.push(n)
+        end
+      end
+    end
+  end
+  
 end
+
+
 
 include Enumerable
 
-[1, 2, 3].each_with_index { |x, y| puts "Current number #{x}, #{y}"}
+# [1, 2, 3, 4, 5].select { |num| puts num.even? }
 
-my_each_with_index([1, 2, 3, 4, 5, 6, 7, 8, 9]) { |x, y| puts "Current number #{x}, #{y}"}
+[1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |num| puts num.even? }
