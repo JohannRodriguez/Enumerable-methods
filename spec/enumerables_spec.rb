179c2b0eab7c137e48ea1489566089c2cb53f98a
Pedro Donda <phfdonda@gmail.com>
2020-04-17 15:39:20 -0300

    Add my_select test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,24 +1,30 @@
-#spec/calculator_spec.rb
+# spec/calculator_spec.rb
 require './lib/enumerable_methods'
 
 describe Enumerable do
-  describe "#my_each" do
-    it "iterates over each item of an array" do
+  describe '#my_each' do
+    it 'iterates over each item of an array' do
       expect([11, 2, 3, 56].my_each { |x| x }).to eql([11, 2, 3, 56])
     end
 
-    it "expecting enumerator if no block is given" do
+    it 'expecting enumerator if no block is given' do
       expect([11, 2, 3, 56].my_each).to be_kind_of(Enumerator)
     end
   end
 
-  describe "#my_each_with_index" do
-    it "iterates over each item and returns item with its index" do
+  describe '#my_each_with_index' do
+    it 'iterates over each item and returns item with its index' do
       expect([11, 2, 3, 56].my_each_with_index { |n, i| [n, i] }).to eql([11, 0, 2, 1, 3, 2, 56, 3])
     end
 
-    it "expecting enumerator if no block is given for the index" do
+    it 'expecting enumerator if no block is given for the index' do
       expect([11, 2, 3, 56].my_each_with_index).to be_kind_of(Enumerator)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns an array of selected items that pass a test' do
+      expect([11, 2, 3, 56].my_select { |x| x > 10 }).to eql([11, 56])
     end
   end
 end
